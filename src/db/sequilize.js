9be811a0cd174fb95f48687c803743e8ca85be83
Anthonynzube <nzubeanthony4@gmail.com>
2021-05-12 13:21:13 +0100

    drop tables

--- src/db/sequilize.js
+++ src/db/sequilize.js
@@ -42,7 +42,7 @@ db.notifications.belongsTo(db.rooms);
 db.notice.belongsTo(db.notifications);
 
 
-db.sequelize.sync();
+db.sequelize.sync({ force: true });
 // {force:true}
 db.sequelize
     .authenticate()
