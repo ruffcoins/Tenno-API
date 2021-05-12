116f6b3f622e15663ee8cf7cb699ad29e3ffc26b
Anthonynzube <nzubeanthony4@gmail.com>
2021-05-12 14:36:11 +0100

    undo all changes

--- src/controllers/properties/properties_controller.js
+++ src/controllers/properties/properties_controller.js
@@ -46,9 +46,7 @@ class PropertiesController {
             }).then((property) => {
 
                 // After creating the property, create all the rooms attached to that property
-                // let roomName = '';
-                let newRoomName = '';
-
+                let roomName = '';
                 for (let i = 0; i < req.body.rooms.length; i++) {
                     roomArray.push(req.body.rooms[i]);
                 }
@@ -57,16 +55,14 @@ class PropertiesController {
 
                     for (let j = 0; j < roomArray[i].number; j++) {
                         if (!Number.isNaN(parseInt(roomArray[i].room_type.charAt(0), 10))) {
-                            // roomName = roomArray[i].room_type;
-                            newRoomName = roomArray[i].room_type;
+                            roomName = roomArray[i].room_type;
                         } else {
-                            // roomName = roomArray[i].room_type.charAt(0);
-                            newRoomName = roomArray[i].room_type;
+                            roomName = roomArray[i].room_type.charAt(0);
                         }
                         Room.create({
                             propertyId: property.id,
                             room_type: roomArray[i].room_type,
-                            room_name: `${newRoomName} (room ${j + 1})`
+                            room_name: `${roomName}room${j + 1}`
 
                         });
                     }
