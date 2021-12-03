73c49d8b7580b54217379ee9aaab813469524001
Anthony Nzube <nzubeanthony4@gmail.com>
2021-12-04 00:01:17 +0100

    notification routes

--- src/routes/notifications/notifications_route.js
+++ src/routes/notifications/notifications_route.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {
-    showAllNotifications, updateNotification
+    createNotification, getAllNotifications, updateNotification, showAllCompletedNotifications, showIncompleteNotifications
     
 } = require('../../controllers/notifications/notifications_controller');
 
 
-router.get('/api/tenno/notifications/showAll', showAllNotifications);
+router.get('/api/tenno/notifications/completed', showAllCompletedNotifications);
+router.get('/api/tenno/notifications/incomplete', showIncompleteNotifications);
 router.put('/api/tenno/notifications/update/:id', updateNotification);
+// router.get('/api/tenno/notifications/create', createNotification);
+router.get('/api/tenno/notifications/showAll', getAllNotifications);
 
 module.exports = router;
 
